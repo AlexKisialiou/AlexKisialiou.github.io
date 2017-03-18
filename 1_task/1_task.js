d448accbe7753d506985e0e8b341357b4050c630
aliaksei_kisialiou <aliaksei.kisialiou@gmail.com>
2017-03-18 12:26:44 +0300

    fix 1_task.js ver 1.2

--- 1_task/1_task.js
+++ 1_task/1_task.js
@@ -1,7 +1,6 @@
 /**
  * Created by Alex on 17.03.2017.
  */
-
 function addition(a, b) {
   return a + b;
 }
@@ -19,22 +18,65 @@ function division(a, b) {
 }
 
 function integerDivision(a, b) {
-  var fraction = division(a, b);
-  return  parseInt(fraction, 10);
+  return parseInt(division(a, b), 10);
 }
-
-console.log(addition(1, -4));
+console.log("--------addition-----------");
+console.log(addition(1, 2));
 console.log(addition(1, "2"));
-console.log(addition("5", "2"));
-console.log(addition(false, true));
-console.log(addition(false, {}));
-console.log(addition(1, "one"));
+console.log(addition("1", "2"));
+console.log(addition(1, "two"));
+console.log(addition("1", "two"));
+console.log(addition(1, false));
+console.log(addition("1", true));
+console.log(addition(1, {}));
 console.log(addition());
 console.log(addition(1));
-console.log(addition(1, 4, 5));
-console.log(subtraction(1, -10));
-console.log(subtraction(1, "-10"));
-console.log(subtraction("-10", "-10"));
-console.log(multiplication(3, 2));
-console.log(division(13, 3));
-console.log(integerDivision(13, 3));
+console.log(addition(1, 2, 5));
+console.log("--------subtraction-----------");
+console.log(subtraction(1, 2));
+console.log(subtraction(1, "2"));
+console.log(subtraction(2, "two"));
+console.log(subtraction("1", "two"));
+console.log(subtraction(1, false));
+console.log(subtraction("1", true));
+console.log(subtraction(1, {}));
+console.log(subtraction("-10", true));
+console.log(subtraction());
+console.log(subtraction(1));
+console.log(subtraction(1, 2, 5));
+console.log("--------multiplication-----------");
+console.log(multiplication(1, 2));
+console.log(multiplication(1, "2"));
+console.log(multiplication(2, "two"));
+console.log(multiplication("1", "two"));
+console.log(multiplication(1, false));
+console.log(multiplication("1", true));
+console.log(multiplication(1, {}));
+console.log(multiplication("-10", true));
+console.log(multiplication());
+console.log(multiplication(1));
+console.log(multiplication(1, 2, 5));
+console.log("--------division-----------");
+console.log(division(1, 2));
+console.log(division(1, "2"));
+console.log(division(2, "two"));
+console.log(division("1", "two"));
+console.log(division(1, false));
+console.log(division("1", true));
+console.log(division(1, {}));
+console.log(division("-10", true));
+console.log(division());
+console.log(division(1));
+console.log(division(1, 2, 5));
+console.log("--------integerDivision-----------");
+console.log(integerDivision(1, 2));
+console.log(integerDivision(1, "2"));
+console.log(integerDivision(2, "two"));
+console.log(integerDivision("1", "two"));
+console.log(integerDivision(1, false));
+console.log(integerDivision("1", true));
+console.log(integerDivision(1, {}));
+console.log(integerDivision("-10", true));
+console.log(integerDivision());
+console.log(integerDivision(1));
+console.log(integerDivision(1, 2, 5));

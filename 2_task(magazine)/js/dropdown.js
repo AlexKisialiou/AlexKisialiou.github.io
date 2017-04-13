f94b7c169323841de675377f2ff14efed122e8fe
aliaksei_kisialiou <aliaksei.kisialiou@gmail.com>
2017-04-13 20:55:47 +0300

    add some part of layout and fix

--- 2_task(magazine)/js/dropdown.js
+++ 2_task(magazine)/js/dropdown.js
@@ -1,24 +1,32 @@
-function myFunction(elem) {
-  document.getElementById(elem.getAttribute("data-name")).classList.toggle("show");
+
+addListenerForElements();
+
+function addListenerForElements() {
+  let arrayOfClass = document.getElementsByClassName("dropbtn");
+  for (let i = 0; i < arrayOfClass.length; i++) {
+    arrayOfClass[i].addEventListener("click", function() {
+        document.getElementById(arrayOfClass[i].getAttribute("data-dropdown-menu")).classList.toggle("show");
+    } );
+  }
 }
 
 window.onclick = function(event) {
-  var obj = event.target;
+  let obj = event.target;
   let dropdowns = document.getElementsByClassName("drop");
-  if (!obj.hasAttribute("data-name")) {
+  if (!obj.hasAttribute("data-dropdown-menu")) {
     for (let i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   } else {
     for (let i = 0; i < dropdowns.length; i++) {
-      let ulId = document.getElementById(obj.getAttribute("data-name"));
+      let ulId = document.getElementById(obj.getAttribute("data-dropdown-menu"));
       let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
+      if (openDropdown.classList.contains("show")) {
         if (openDropdown.getAttribute("id") === ulId.getAttribute("id")) continue;
-        openDropdown.classList.remove('show');
+        openDropdown.classList.remove("show");
       }
     }
   }
